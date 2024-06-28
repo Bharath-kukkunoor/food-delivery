d17eafb4c05faf232797d2969334e9a638f04fce
Bharath-kukkunoor <bharathkukkunoor@gmail.com>
2024-06-28 17:23:50 +0530

    abc

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Home.css'
 import Header from '../../Components/Header/Header'
 import ExploreMenu from '../../Components/ExploreMenu/ExploreMenu'
 
 const Home = () => {
+    const [catergory,setCategory]=useState("All");
+
   return (
     <div>
       <Header/>
-      <ExploreMenu/>
+      <ExploreMenu catergory={catergory} setCategory={setCategory}/>
     </div>
   )
 }
